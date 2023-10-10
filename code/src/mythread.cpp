2772db846cbe38691672bd2508d796e9244e85bb
kevin.auberson <kevin.auberson@heig-vd.ch>
2023-10-10 13:58:30 +0200

    Update MyThread class with constructor et creation of thread

--- code/src/mythread.cpp
+++ code/src/mythread.cpp
@@ -1,22 +1,11 @@
 #include "mythread.h"
 
-MyThread::MyThread(long long unsigned int nbToCompute, unsigned int nbChars, const std::string& charset, const int numThreads, const std::string& salt, const std::string& hash){
-    this->nbToCompute = 0;
-    this->i = 0;
-    this->nbComputed = nbToCompute;
-    this->nbValidChars = charset.length();
-    this->currentPasswordString  = currentPasswordString.fill(charset.at(0), this->nbValidChars);
-    this->currentPasswordArray.fill(0,nbChars);
-    this->md5 = new QCryptographicHash(QCryptographicHash::Md5);
-    this->nbThreads = numThreads;
-}
-void MyThread::startThreads(){
+MyThread::MyThread(int nbThreads){
+    this->nbThreads = nbThreads;
     for (int i = 0; i < this->nbThreads; ++i)
-        this->threads.push_back(new PcoThread (workThread, i));
+        this->threads.push_back(new PcoThread (&MyThread::workThread, i));
 }
 
 void MyThread::workThread(int threadID){
-    while (true) {
 
-    }
 }
