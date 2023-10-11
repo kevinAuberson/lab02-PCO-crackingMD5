80da4371065b02aee9e999bd5f9a62b86c78c287
kevin.auberson <kevin.auberson@heig-vd.ch>
2023-10-11 11:16:28 +0200

    Update

--- code/src/mythread.cpp
+++ code/src/mythread.cpp
@@ -1,11 +1,31 @@
 #include "mythread.h"
 
-MyThread::MyThread(int nbThreads){
-    this->nbThreads = nbThreads;
+MyThread::MyThread(unsigned int nbThread, unsigned int nbToCompute, unsigned int nbChars, const QString& salt, const QString& hash, const QString& charset, int nbValidChars){
+    this->nbToCompute = nbToCompute;
+    this->nbComputed = 0;
+    this->nbValidChars = charset.length();
+    this->currentPasswordString.fill(charset.at(0), nbChars);
+    this->currentPasswordArray.fill(0, nbChars);
+    this->md5 = new QCryptographicHash(QCryptographicHash::Md5);
+    this->hash = hash;
+    this->nbThreads = nbThread;
+}
+
+void MyThread::startThread(){
     for (int i = 0; i < this->nbThreads; ++i)
         this->threads.push_back(new PcoThread (&MyThread::workThread, i));
 }
+QString MyThread::hashing(){
+    /* On vide les données déjà ajoutées au générateur */
+    this->md5.reset();
+    /* On préfixe le mot de passe avec le sel */
+    md5.addData(salt.toLatin1());
+    md5.addData(currentPasswordString.toLatin1());
+    /* On calcul le hash */
+    QString currentHash = md5.result().toHex();
 
-void MyThread::workThread(int threadID){
+    return currentHash;
+}
+QString MyThread::workThread(int threadID){
 
 }
--- code/src/mythread.h
+++ code/src/mythread.h
@@ -3,13 +3,27 @@
 
 #include <pcosynchro/pcothread.h>
 #include <QVector>
+#include <QCryptographicHash>
 
 class MyThread{
 private:
     int nbThreads;
     QVector<PcoThread*> threads;
+    unsigned int i;
+    long long unsigned int nbToCompute;
+    long long unsigned int nbComputed;
+    unsigned int nbValidChars;
+    QString currentPasswordString;
+    QVector<unsigned int> currentPasswordArray;
+    QString currentHash;
+    QString hash;
+    QString salt;
+    QString charset;
+    QCryptographicHash *md5;
 public:
-    MyThread(int nbThreads);
+    MyThread(unsigned int nbThread, unsigned int nbToCompute, unsigned int nbChars, const QString& salt, const QString& hash, const QString& charset, int nbValidChars);
+    void startThread();
+    QString hashing();
     void workThread(int threadID);
 };
 
